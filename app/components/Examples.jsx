5c5a912f3a30a9216d0b32e9b8d469e6880c2301
jahid284 <amijahid284@gmail.com>
2017-09-25 13:20:14 +0600

    example page change

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,12 @@
 const React = require('react');
 const Examples = (props) => {
   return(
-    <h3>Examples component</h3>
+    <div className="examples">
+      <h3>Examples component</h3>
+      <ol>
+        <li><Link to="/?q=Dhaka,BD">Dhaka</Link></li>
+      </ol>
+    </div>
   );
 }
 
